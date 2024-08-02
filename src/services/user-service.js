4ba7cc05ebb2f10609b95974509f6d8e7255609d
parthoprotim1111 <parthoprotim1112@gmail.com>
2024-08-03 00:50:51 +1000

    added expiry error handling

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -71,6 +71,10 @@ async function isAuthenticated(token) {
         if (error.name === 'JsonWebTokenError') {
             throw new AppError('Invalid JWT token', StatusCodes.BAD_REQUEST);
         }
+        if(error.name == 'TokenExpiredError'){
+
+            throw new AppError(' JWT token Expired', StatusCodes.BAD_REQUEST);
+        }
         console.log(error);
         throw new AppError("JWT token not found", StatusCodes.BAD_REQUEST);
     }
